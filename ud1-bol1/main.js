2d7efacb720ab89fe3c209752058ec135d66ea61
rayelus5 <rayelusanom828@gmail.com>
2025-09-20 16:12:46 +0200

    ejercicio 1

--- ud1-bol1/main.js
+++ ud1-bol1/main.js
@@ -14,11 +14,14 @@ function ejecutarEjercicio(num) {
   output.innerHTML = `<strong>Se ejecutó el ejercicio ${num}...</strong><br><hr><br>`;
 
   switch (num) {
+    case 1:
+      ejercicio1();
+      break;
     case 12:
       ejercicio12();
       break;
     default:
-      output.innerHTML += "Ejercicio no hecho todavía.";
+      output.innerHTML += '<strong style="color: red;">[X]</strong> Ejercicio no hecho todavía.';
   }
 }
 
@@ -28,12 +31,30 @@ function ejecutarEjercicio(num) {
    ========================
 */
 
+function ejercicio1() {
+  let edad = parseInt(prompt("Introduzca su edad: "));
+  let nombre = prompt("Introduzca su nombre: ");
+  let estadoCivil = prompt("Introduzca su estado civil (solter@, casad@, viud@, divorciad@): ");
+
+  let opcionesValidas = ['soltero', 'soltera', 'casado', 'casada', 'viudo', 'viuda', 'divorciado', 'divorciada']
+
+  if (isNaN(edad) || edad <= 0) {
+    output.innerHTML += "[ERROR] Edad no válida.<br>";
+  } else if (!nombre || nombre.trim() === "") { //con trim quitamos los espacios
+    output.innerHTML += "[ERROR] Nombre no válido.<br>";
+  } else if (!estadoCivil || !opcionesValidas.includes(estadoCivil.toLowerCase())) {
+    output.innerHTML += "[ERROR] Estado civil no válido.<br>";
+  } else {
+    output.innerHTML += `Su nombre es ${nombre}, tiene ${edad} años y su estado civil es: ${estadoCivil}`;
+  }
+}
+
 function ejercicio12() {
   let a = parseFloat(prompt("Introduce el primer número:"));
   let b = parseFloat(prompt("Introduce el segundo número:"));
 
   if (isNaN(a) || isNaN(b)) {
-    output.innerHTML += "⚠️ Debes introducir números válidos.";
+    output.innerHTML += "[ERROR] Debes introducir números válidos.";
     return;
   }
 
